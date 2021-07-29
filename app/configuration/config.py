dfc21db3f7add9f46ee26bef42dc420bbf76d64a
nidhogg <j.carlosglxg@gmail.com>
2021-07-29 17:08:37 -0500

    feature(configuration): add a basic configuration

---
+++ app/configuration/config.py
@@ -0,0 +1,40 @@
+from os import environ
+from dataclasses import dataclass
+from os.path import join, abspath
+
+base_dir = abspath(__file__)
+base_db = join(base_dir, "base.db")
+
+
+@dataclass
+class BaseConfig(object):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + base_db
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPLICATION_ROOT = "/api"
+
+
+@dataclass
+class DevConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + base_db
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPLICATION_ROOT = "/api"
+
+
+@dataclass
+class DeployConfig(BaseConfig):
+    DEBUG = False
+    TESTING = False
+    if environ.get("PATH_DB"):
+        SQLALCHEMY_DATABASE_URI = environ.get("PATH_DB")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPLICATION_ROOT = "/api"
+
+
+configurations = {
+    "DEV": DevConfig,
+    "DEPLOY": DeployConfig
+}
