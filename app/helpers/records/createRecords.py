783e1678cebc21be89a4e595bdc882d5480896d9
nidhogg <j.carlosglxg@gmail.com>
2021-07-30 16:07:16 -0500

    feature(Records): add to scripts for the creations of records

---
+++ app/helpers/records/createRecords.py
@@ -0,0 +1,20 @@
+from app.models.logModel import LogModel
+from app.models.fatherModel import FatherModel
+from app.models.childrenModel import ChildrenModel
+from app.helpers.records.dummyRecords import get_dummy_record
+from app.models import db
+
+
+def create_records(size: int):
+    for n in range(1, size+1):
+        new_log = get_dummy_record(n, LogModel)
+        db.session.add(new_log)
+        db.session.commit()
+
+        new_father = get_dummy_record(n, FatherModel)
+        db.session.add(new_father)
+        db.session.commit()
+
+        new_children = get_dummy_record(n, ChildrenModel)
+        db.session.add(new_children)
+        db.session.commit()
---
+++ app/helpers/records/dummyRecords.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from sqlalchemy import DateTime, Integer, Float, String, Boolean
+
+
+def get_dummy_record(n: int, some_class):
+    new_record = some_class()
+    for key in some_class.__table__.columns.keys():
+        type_column = some_class.__table__.columns[key].type
+        if isinstance(type_column, Integer) and key != "id":
+            setattr(new_record, key, n)
+        elif isinstance(type_column, Float):
+            setattr(new_record, key, float(f"{n}.{n}"))
+        elif isinstance(type_column, String):
+            setattr(new_record, key, f"{n}")
+        elif isinstance(type_column, Boolean):
+            setattr(new_record, key, True)
+        elif isinstance(type_column, DateTime):
+            setattr(new_record, key, datetime.now())
+        else:
+            print(f"Enable to set a default value for {type_column}")
+    return new_record
