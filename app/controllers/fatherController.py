19f8692a36e4e62099d3a101a024cd394c7e69f7
nidhogg <j.carlosglxg@gmail.com>
2021-07-30 16:04:54 -0500

    feature(Controller): add controllers to the models

---
+++ app/controllers/fatherController.py
@@ -0,0 +1,8 @@
+from app.controllers.baseController import BaseController
+from app.models.fatherModel import FatherModel
+from app.schemas.fatherSchema import FatherSchema
+
+
+class FatherController(BaseController):
+    model = FatherModel
+    schema = FatherSchema
