00f5cc9492cbcde0d93bd21516e83f4fe93017a8
nidhogg <j.carlosglxg@gmail.com>
2021-07-30 19:13:38 -0500

    refact(Routes): Remove hard code with the new config file

--- app/routes/childrenRoute.py
+++ app/routes/childrenRoute.py
@@ -1,25 +1,27 @@
 from flask import Blueprint
 from app.controllers.childrenController import ChildrenController
+from app.config.configEndpoint import ConfigurationEndpoint
 
 
 children_blueprint = Blueprint("children", __name__)
+endpoint = ConfigurationEndpoint.endpoint_children
 
 
-@children_blueprint.route("/children", methods=["POST"])
+@children_blueprint.route(f"{endpoint}", methods=["POST"])
 def new_children():
     return ChildrenController().create_record()
 
 
-@children_blueprint.route("/children/<id_record>", methods=["PATCH"])
-def update_children():
-    return ChildrenController().update_record()
+@children_blueprint.route(f"{endpoint}/<id_record>", methods=["PATCH"])
+def update_children(id_record):
+    return ChildrenController().update_record(id_record)
 
 
-@children_blueprint.route("/children", methods=["GET"])
+@children_blueprint.route(f"{endpoint}", methods=["GET"])
 def get_all_children():
     return ChildrenController().get_all_records()
 
 
-@children_blueprint.route("/children/<id_record>", methods=["GET"])
-def get_children():
-    return ChildrenController().get_individual_record()
+@children_blueprint.route(f"{endpoint}/<id_record>", methods=["GET"])
+def get_children(id_record):
+    return ChildrenController().get_individual_record(id_record)
--- app/routes/fatherRoute.py
+++ app/routes/fatherRoute.py
@@ -1,25 +1,27 @@
 from flask import Blueprint
 from app.controllers.fatherController import FatherController
+from app.config.configEndpoint import ConfigurationEndpoint
 
 
 father_blueprint = Blueprint("father", __name__)
+endpoint = ConfigurationEndpoint.endpoint_father
 
 
-@father_blueprint.route("/father", methods=["POST"])
+@father_blueprint.route(f"{endpoint}", methods=["POST"])
 def new_father():
     return FatherController().create_record()
 
 
-@father_blueprint.route("/father/<id_record>", methods=["PATCH"])
-def update_father():
-    return FatherController().update_record()
+@father_blueprint.route(f"{endpoint}/<id_record>", methods=["PATCH"])
+def update_father(id_record):
+    return FatherController().update_record(id_record)
 
 
-@father_blueprint.route("/father", methods=["GET"])
+@father_blueprint.route(f"{endpoint}", methods=["GET"])
 def get_fathers():
     return FatherController().get_all_records()
 
 
-@father_blueprint.route("/father/<id_record>", methods=["GET"])
-def get_father():
-    return FatherController().get_individual_record()
+@father_blueprint.route(f"{endpoint}/<id_record>", methods=["GET"])
+def get_father(id_record):
+    return FatherController().get_individual_record(id_record)
