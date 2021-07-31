ed3ed8fb2f102954a84181da18e6932beade4007
nidhogg <j.carlosglxg@gmail.com>
2021-07-30 19:19:11 -0500

    refact(Controllers): a better use of the return message

--- app/controllers/databaseController.py
+++ app/controllers/databaseController.py
@@ -1,6 +1,9 @@
 from app.models import db, load_models
 from app.helpers.records.createRecords import create_records
 from app.messages.returnMessages import MessageReturn
+from app.messages.statusMessages import STATUS_200, STATUS_500
+from app.messages.errorMessages import ERROR_MSG_500
+from app.messages.successMessages import SUCCESS_MSG_200
 from flask import Response
 
 
@@ -11,10 +14,10 @@ class DatabaseController:
             load_models()
             db.create_all()
             create_records(10)
-            return MessageReturn().create_record_message("Data")
+            return MessageReturn().custom_return_message("", "Database created", STATUS_200)
         except Exception as error:
             db.session.rollback()
-            return MessageReturn().error_id_not_found()
+            return MessageReturn().custom_return_message("", "Error: Unable to create database", STATUS_500)
 
     def delete_database(self) -> Response:
         try:
@@ -22,7 +25,7 @@ class DatabaseController:
                 db.session.execute(f"TRUNCATE TABLE {table} CASCADE")
                 db.session.execute(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1")
             db.session.commit()
-            return MessageReturn().create_record_message("Data")
+            return MessageReturn().custom_return_message("", "Database deleted", STATUS_200)
         except Exception as error:
             db.session.rollback()
-            return MessageReturn().error_id_not_found()
+            return MessageReturn().custom_return_message("", "Error: Unable to delete database", STATUS_500)
--- app/controllers/healthCheckServerController.py
+++ app/controllers/healthCheckServerController.py
@@ -1,7 +1,8 @@
 from app.messages.returnMessages import MessageReturn
+from app.messages.statusMessages import STATUS_200
 from flask import Response
 
 
 class HealthCheckServerController:
     def check_server(self) -> Response:
-        return MessageReturn().access_record_message("")
+        return MessageReturn().custom_return_message("", "Ok server", STATUS_200)
