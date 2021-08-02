8b8d4d1157212fd51ebf7016a718d06dd798f05e
nidhogg <j.carlosglxg@gmail.com>
2021-08-02 12:25:01 -0500

    feature(Models): Change models, now are One to Many Bidirectional

--- app/models/childrenModel.py
+++ app/models/childrenModel.py
@@ -5,9 +5,10 @@ from app.models import db
 class ChildrenModel(db.Model, SerializerMixin):
     __tablename__ = "children_model"
     __table_args__ = {"extend_existing": True}
-    serializer_rules = ()
+    serialize_rules = ("-father.children", )
 
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(50), unique=True, nullable=False)
     age: int = db.Column(db.Integer)
-    parent_id: int = db.Column(db.Integer, db.ForeignKey("father_model.id"))
+    father_id: int = db.Column(db.Integer, db.ForeignKey("father_model.id", ondelete="CASCADE"), nullable=False)
+    father = db.relationship("FatherModel", back_populates="children")
--- app/models/fatherModel.py
+++ app/models/fatherModel.py
@@ -5,9 +5,9 @@ from app.models import db
 class FatherModel(db.Model, SerializerMixin):
     __tablename__ = "father_model"
     __table_args__ = {"extend_existing": True}
-    serializer_rules = ()
+    serialize_rules = ("-children.father", )
 
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(50), unique=True, nullable=False)
     age: int = db.Column(db.Integer)
-    children = db.relationship("ChildrenModel", cascade="all, delete-orphan")
+    children = db.relationship("ChildrenModel", back_populates="father")
