bae28e532a0067cf1068dc01ad6d4e37f0f9fbf0
nidhogg <j.carlosglxg@gmail.com>
2021-08-04 11:55:14 -0500

    feature(DeleteRecords): add a functionality for delete records

---
+++ app/helpers/records/deleteRecords.py
@@ -0,0 +1,9 @@
+from app.models.entryORM import db
+from typing import NoReturn
+
+
+def delete_records() -> NoReturn:
+    for table in db.metadata.tables.keys():
+        db.session.execute(f"TRUNCATE TABLE {table} CASCADE")
+        db.session.execute(f"ALTER SEQUENCE {table}_id_seq RESTART WITH 1")
+    db.session.commit()
