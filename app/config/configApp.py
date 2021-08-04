11ee44778d8ac2b7fdfc7478ef7d4a5252b9edb9
nidhogg <j.carlosglxg@gmail.com>
2021-08-04 11:55:55 -0500

    feature(Config): add a configuration for the database

--- app/config/configApp.py
+++ app/config/configApp.py
@@ -1,16 +1,13 @@
 from os import environ
 from dataclasses import dataclass
-from os.path import join, abspath, dirname
-
-base_dir = abspath(dirname(__file__))
-base_db = join(base_dir, "base.db")
+from app.config.configDB import DBConfig, DBDevConfig, DBDeployConfig
 
 
 @dataclass
 class AppConfig(object):
     DEBUG: bool = True
     TESTING: bool = True
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///" + base_db
+    SQLALCHEMY_DATABASE_URI: str = DBConfig.PATH
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     APPLICATION_ROOT: str = "/api"
 
@@ -19,19 +16,14 @@ class AppConfig(object):
 class AppDevConfig(AppConfig):
     DEBUG: bool = False
     TESTING: bool = False
-    SQLALCHEMY_DATABASE_URI: str = "sqlite:///" + base_db
-    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
-    APPLICATION_ROOT: str = "/api"
+    SQLALCHEMY_DATABASE_URI: str = DBDevConfig.PATH
 
 
 @dataclass
 class AppDeployConfig(AppConfig):
     DEBUG: bool = False
     TESTING: bool = False
-    if environ.get("PATH_DB"):
-        SQLALCHEMY_DATABASE_URI: str = environ.get("PATH_DB")
-    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
-    APPLICATION_ROOT: str = "/api"
+    SQLALCHEMY_DATABASE_URI: str = DBDeployConfig.PATH
 
 
 config_app: dict = {"DEV": AppDevConfig, "DEPLOY": AppDeployConfig}
